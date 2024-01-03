3ac0dd9886600a87c132bcd514752cddc05eca15
IbrahimGomaa10 <ibrahim.gomaa1013@gmail.com>
2024-01-03 15:31:23 +0200

    uploaded for https

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ const App = () => {
     0
   );
   useEffect(() => {
-    fetch("http://localhost:8000/questions")
+    fetch("https://localhost:8000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
