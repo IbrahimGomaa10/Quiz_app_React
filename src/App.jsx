31fafa48a932393fa02ea80fcbfe7e91d421dc80
IbrahimGomaa10 <ibrahim.gomaa1013@gmail.com>
2024-01-03 16:51:31 +0200

    Updated_Json_Server

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ const App = () => {
     0
   );
   useEffect(() => {
-    fetch("http://localhost:8000/questions")
+    fetch("https://json-quiz-server.vercel.app/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
