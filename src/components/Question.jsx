c3180b5b21525d7e247a6aaf66b807cbe2921022
IbrahimGomaa10 <ibrahim.gomaa1013@gmail.com>
2024-01-03 14:58:52 +0200

    Uploaded after i clear clg

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,8 +1,6 @@
 import Options from "./Options";
 
 function Question({ question, answer, dispatch }) {
-  console.log(question);
-
   return (
     <div>
       <Options question={question} answer={answer} dispatch={dispatch} />
